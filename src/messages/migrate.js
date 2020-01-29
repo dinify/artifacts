725bb0fea542fc72eaee67b0af10367a026d4ad5
Máté Homolya <mate.homolya@gmail.com>
2020-01-29 10:31:22 +0100

    Bump cldr version, migration scripts

--- src/messages/migrate.js
+++ src/messages/migrate.js
@@ -1,20 +1,16 @@
 require("dotenv").config();
 const yargs = require("yargs");
 const fs = require("fs");
-const { pipe, toPairs, fromPairs } = require("ramda");
+const { pipe, toPairs, fromPairs, keys, values } = require("ramda");
 const { deflatten, flatten } = require("../lib/json");
 const { stringify } = require("../lib/es6");
 const config = require("../build/config");
+const cfg = require("./migration-1");
 
 const argv = yargs
   .option("language", {
     alias: "l",
     description: "The language to migrate, defaults to all",
-    type: "string"
-  })
-  .option("namespace", {
-    alias: "n",
-    description: "The namespace to migrate to core",
     type: "string"
   })
   .option("keys", {
@@ -37,21 +33,24 @@ const argv = yargs
 
 config.include = {
   languages: argv.language ? [argv.language] : config.namespaces.core,
-  namespaces: [argv.namespace],
   keys: argv.keys,
   output: argv.output
 };
 config.sourceLanguage = argv.source || "en";
-
-config.include.namespaces.forEach(ns => {
-  console.log("Migrating languages: ", config.include.languages);
+keys(cfg).forEach(ns => {
+  console.log(`Migrating languages for ${ns}: `, config.include.languages);
   config.include.languages.forEach(language => {
     const core = require(`../i18n/messages/${language}/core`);
     const source = require(`../i18n/messages/${language}/${ns}`);
-    const keys = config.include.keys || schema;
-    const add = toPairs(flatten(source)).filter(([k, v]) => keys.includes(k));
+    const sourceKeys = keys(cfg[ns]);
+    const resultKeys = values(cfg[ns]);
+    const add = toPairs(flatten(source))
+      .filter(([k, v]) => sourceKeys.includes(k))
+      .map(([k, v]) => [resultKeys[sourceKeys.findIndex(a => a === k)], v]);
     const keep = deflatten(
-      fromPairs(toPairs(flatten(source)).filter(([k, v]) => !keys.includes(k)))
+      fromPairs(
+        toPairs(flatten(source)).filter(([k, v]) => !sourceKeys.includes(k))
+      )
     );
     const result = deflatten(fromPairs([...add, ...toPairs(flatten(core))]));
     const output = config.include.output || `./src/i18n/messages`;
---
+++ src/messages/migration-1.js
@@ -0,0 +1,20 @@
+module.exports = {
+  app: {
+    subtotal: "price.subtotal",
+    total: "price.total",
+    totalConverted: "price.totalConverted"
+  },
+  waiterboard: {
+    addon: "menu.addon",
+    price: "price.title",
+    total: "price.total",
+    "by-card": "price.card",
+    "by-cash": "price.cash"
+  },
+  dashboard: {
+    "menu.price": "menu.price",
+    "menu.addons": "menu.addons",
+    "menu.choices": "menu.choices",
+    "menu.ingredients": "menu.ingredients"
+  }
+};
